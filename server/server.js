191a60ae3fb3cdc50da93b4244501efffee15456
daniel de jong <daniel@MacBook-Pro.local>
2019-01-24 10:52:44 +0100

    Add get todos by id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+let { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose");
 var { todo } = require("./models/todo");
@@ -29,6 +30,24 @@ app.get("/todos", (req, res) => {
     }
   );
 });
+app.get("/todos/:id", (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send("Invalid ID");
+  }
+  todo
+    .findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send("Id not found");
+      }
+      res.send(todo);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
 app.listen(3001, () => {
   console.log("Connected to http://localhost:3001");
 });
